b3640b6a8f548448ed370e2ee9877ccf1c5c4af1
Tariselan <woodmoreaaron@gmail.com>
2022-04-15 00:49:15 +1200

    Added Vital Feature

--- artlangs/ablom/main.js
+++ artlangs/ablom/main.js
@@ -1,3 +1,10 @@
 function projectLink() {
     window.location.replace("../projects.html");
 }
+var bell = new Audio("../bell.mp3");
+function secret() {
+    bell.play();
+    setTimeout(function move() {
+        window.location.replace("../poyeeni_na/mainpage.html");
+    }, 650);
+}
