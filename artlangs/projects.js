5c9b42c857c75e764f7360bdf2ac4581684a4970
Tariselan <woodmoreaaron@gmail.com>
2022-10-24 23:11:18 +1300

    Major Optimisations to Links

--- artlangs/projects.js
+++ artlangs/projects.js
@@ -1,12 +1,45 @@
-function poyeeniNa() {
-    window.location.replace("poyeeni_na/mainpage.html");
+const languages = [
+    "poyeeni_na/main.html", // 0
+    "identity_lang/main.html", // 1
+    "kawa/main.html", // 2
+    "ablom/main.html", // 3
+    "nicesounds/main.html" // 4
+];
+const langDict = [
+    "poyeeni_na",
+    "identity_lang",
+    "kawa",
+    "ablom",
+    "nicesounds"
+]
+var bell = new Audio("../bell.mp3");
+var conlang = location.href.split("/").slice(-2)[0];
+
+function language(x) {
+    if (x == "a") {
+        let location = window.location;
+        alert(location)
+        //window.location.replace("../projects.html");
+    }
+    else {
+        window.location.replace(languages[x]);
+    }
 }
-function identity() {
-    window.location.replace("identity_lang/grammardoc.html");
-}
-function reverse() {
-    window.location.replace("reverse_abugida/grammardocument.html");
-}
-function ablom() {
-    window.location.replace("ablom/main.html");
+function secret() {
+    bell.play();
+    var conlang = location.href.split("/").slice(-2)[0];
+    setTimeout(function move() {
+        if (conlang == 'poyeeni_na') {
+            window.location.replace('../ablom/main.html');
+        }
+        else if (conlang == 'ablom') {
+            window.location.replace('../nicesounds/main.html');
+        }
+        else if (conlang == 'nicesounds') {
+            window.location.replace('../kawa/main.html');
+        }
+        else if (conlang == 'kawa') {
+            window.location.replace('../poyeeni_na/mainpage.html');
+        }
+    }, 650);
 }
